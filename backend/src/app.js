8376e51ecdd1f524a88911a27cf5151619781e81
Joel Kadugula <kadugulajoel@gmail.com>
2026-06-02 12:49:15 +0530

    prepared for the deployment

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,16 +1,36 @@
-import express from 'express'
-import cors from 'cors'
+import express from "express";
+import cors from "cors";
+import config from "./config/config.js";
+import path from 'path'
 
-import notesRoutes from './routes/notesRoutes.js'
+import notesRoutes from "./routes/notesRoutes.js";
 import rateLimiter from "./middlewares/rateLimiter.js";
 
-const app = express()
+const app = express();
+const __dirname = path.resolve();
 
-app.use(cors())
-app.use(express.json())
-app.use(rateLimiter)
+// middleware
+if (config.NODE_ENV !== "production") {
+  app.use(
+    cors({
+      origin: "http://localhost:5173",
+    }),
+  );
+}
 
-app.use('/api/notes', notesRoutes)
+app.use(express.json());
+app.use(rateLimiter);
 
+app.use("/api/notes", notesRoutes);
 
-export default app
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
+
+app.use("/api/notes", notesRoutes);
+
+export default app;
--- backend/src/config/config.js
+++ backend/src/config/config.js
@@ -22,7 +22,8 @@ const config = {
   MONGO_URI: process.env.MONGO_URI,
   PORT: process.env.PORT,
   UPSTASH_REDIS_REST_URL: process.env.UPSTASH_REDIS_REST_URL,
-  UPSTASH_REDIS_REST_TOKEN: process.env.UPSTASH_REDIS_REST_TOKEN
+  UPSTASH_REDIS_REST_TOKEN: process.env.UPSTASH_REDIS_REST_TOKEN,
+  NODE_ENV: process.env.NODE_ENV
   // JWT_SECRET: process.env.JWT_SECRET,
 };
 
